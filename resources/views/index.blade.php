0ebb7bcad59f17a800be95e3fa83f6fda219ed3c
masayoshi <tomoyasu1drums@gmail.com>
2024-06-18 23:28:07 +0900

    first commit: README.md(構成)の作成・記入

---
+++ resources/views/index.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+</head>
+<body>
+    <div>
+        <p>国、クラブチームのタグを作る。
+            初期表示は、とりあえず、国にする。
+            カラム一覧：id, country_name
+
+            クラブチーム
+            カラム一覧：id, club_team_name, (理想)プレイヤー数
+        </p>
+    </div>
+
+    <div>
+        - 国
+            - 国名
+            - プレイヤー
+                - クラブチーム
+                - age
+                - position
+                - 代表通算ゴール数
+                - 代表通算アシスト数
+                - 過去の所属クラブチーム
+            - W杯優勝回数
+
+        - クラブチーム
+            - プレイヤー名一覧
+                - 国
+                - age
+                - position
+                - 通算ゴール数
+                - 通算アシスト数
+                - 過去の所属チーム
+            - プレイヤー数
+            - リーグ優勝回数
+    </div>
+
+    <nav>
+        <ul>
+            <li>Countries</li>
+            <li>Club Teams</li>
+        </ul>
+    </nav>
+
+    <p>{{ $msg }}</p>
+</body>
+</html>
